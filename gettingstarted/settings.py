b92e1ee69437f0d8d0461dff224fe3250b3cf3c7
westonewinner <47166843+westonewinner@users.noreply.github.com>
2019-09-07 23:42:28 +0000

    Update settings.py

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'stpq)sy@y5ysioo(fm0*tao86kp0wdps)+@lyp-t_pjy08f%w5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
